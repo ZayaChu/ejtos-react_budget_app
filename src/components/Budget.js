ac0b11afc825def95b6d4bb2e77fcaab5e543089
ZayaChu <158593066+ZayaChu@users.noreply.github.com>
2024-02-08 22:48:28 -0500

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,10 +2,21 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
+    
+        const { expenses } = useContext(AppContext);
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total += item.cost);
+        }, 0);
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
+        isGreater(event.target.value);
+    }
+    function isGreater(number){
+        if(number<totalExpenses){
+            alert("You cannot reduce the budget value lower than the speanding");
+        }
     }
     return (
 <div className='alert alert-secondary'>
